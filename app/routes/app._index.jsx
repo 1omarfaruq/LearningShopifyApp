46872cabc329c80ffa8f6b0089e7cd0bae5d7bc5
iLMIFY <ilmify@iLMIFYs-Mac-mini.local>
2026-08-09 16:57:08 +0600

    Theme extension 555 created

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -131,7 +131,7 @@ export default function Index() {
   const generateProduct = () => fetcher.submit({}, { method: "POST" });
 
   return (
-    <s-page heading="Shopify app template 44">
+    <s-page heading="Shopify app template">
       <s-button slot="primary-action" onClick={generateProduct}>
         Generate a product
       </s-button>
--- app/routes/app.reviews/route.jsx
+++ app/routes/app.reviews/route.jsx
@@ -2,25 +2,12 @@ export default function Reviews() {
   return (
     <s-page heading="Reviews">
       <s-section heading="Buttons">
-        <s-button>Primary</s-button>
-        <s-button variant="secondary">Secondary</s-button>
-        <s-button variant="destructive">Destructive</s-button>
-        <s-button variant="outline">Outline</s-button>
-        <s-button disabled="true">Disabled</s-button>
-        <br />
-        <s-button variant="primary" icon="code" tone="critical">
-          Replace Icon
-        </s-button>
-        <br />
-        <s-button commandFor="actions-popover">More actions</s-button>
-
-        <s-popover id="actions-popover">
-          <s-stack direction="block">
-            <s-button variant="tertiary">Export products</s-button>
-            <s-button variant="tertiary">Import products</s-button>
-            <s-button variant="tertiary">Print labels</s-button>
-          </s-stack>
-        </s-popover>
+        <s-stack direction="inline" justifyContent="space-around">
+          <s-button>Default</s-button>
+          <s-button variant="primary">Primary</s-button>
+          <s-button variant="secondary">Secondary</s-button>
+          <s-button variant="danger">Danger</s-button>
+        </s-stack>
       </s-section>
     </s-page>
   );
